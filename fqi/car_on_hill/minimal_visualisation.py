f7dd73eb66d83e01f11df60cb7d3b7fbbd5e15de
ptekou <pako.tekou@inria.fr>
2026-07-22 22:03:38 +0000

    minimal visu

--- fqi/car_on_hill/minimal_visualisation.py
+++ fqi/car_on_hill/minimal_visualisation.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-J = np.load("logs/neural_boosted_curriculum/J.npy")
-Q = np.load("logs/neural_boosted_curriculum/Q.npy")
+J = np.load("logs/neural_no_boosted_curriculum/J.npy")
+Q = np.load("logs/neural_no_boosted_curriculum/Q.npy")
 
 J_flat = np.concatenate([J[:, i, :] for i in range(J.shape[1])], axis=-1)
 Q_flat = np.concatenate([Q[:, i, :] for i in range(Q.shape[1])], axis=-1)
@@ -12,22 +12,22 @@ fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 4))
 n = J_flat.shape[1]
 x = np.arange(1, n + 1)
 
-ax1.plot(x, J_flat[0], label="BC-NeuralFQI")
+ax1.plot(x, J_flat[0], label="C-NeuralFQI")
 ax1.vlines([20, 40], *ax1.get_ylim(), colors="black", linestyles="--", alpha=0.5)
 ax1.set_xlabel("Iteration")
 ax1.set_ylabel("Cum. Disc. Return")
-ax1.set_title("J (neural_boosted_curriculum)")
+ax1.set_title("J (neural_no_boosted_curriculum)")
 ax1.legend()
 ax1.grid()
 
-ax2.plot(x, Q_flat[0], label="BC-NeuralFQI", color="C0")
+ax2.plot(x, Q_flat[0], label="C-NeuralFQI", color="C0")
 ax2.vlines([20, 40], *ax2.get_ylim(), colors="black", linestyles="--", alpha=0.5)
 ax2.set_xlabel("Iteration")
 ax2.set_ylabel("||Q - Q*||")
-ax2.set_title("Q diff (neural_boosted_curriculum)")
+ax2.set_title("Q diff (neural_no_boosted_curriculum)")
 ax2.legend()
 ax2.grid()
 
 plt.tight_layout()
-plt.savefig("figures/partial_results.pdf")
+plt.savefig("figures/nc.pdf")
 plt.show()

be05c997ef54c5df5eeff8d1f0d4d92fac8e7825
Pako Maxence TEKOU <tekoumaxencelebaron@gmail.com>
2026-07-23 00:02:35 +0200

    job optimization

--- fqi/car_on_hill/run_neural_fqi_jobs.sh
+++ fqi/car_on_hill/run_neural_fqi_jobs.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-#SBATCH --job-name=neural_fqi_car_on_hill
+#SBATCH --job-name=neural_fqi_%a
 #SBATCH --partition=tau
-#SBATCH --time=7-00:00:00
+#SBATCH --time=1-00:00:00
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=32G
-#SBATCH --output=slurm/logs/%x_%j.out
-#SBATCH --error=slurm/logs/%x_%j.err
+#SBATCH --mem=16G
+#SBATCH --output=slurm/logs/%x_%A_%a.out
+#SBATCH --error=slurm/logs/%x_%A_%a.err
+#SBATCH --array=0-3
 
 export PYTHONPATH=$PYTHONPATH:$PWD/../..
 
-python run.py --n-exp 1 --n-jobs 1 --use-curriculum --use-boosting --use-neural
-python run.py --n-exp 1 --n-jobs 1 --use-boosting --use-neural
-python run.py --n-exp 1 --n-jobs 1 --use-curriculum --use-neural
-python run.py --n-exp 1 --n-jobs 1 --use-neural
-python visualize_neural_results.py
+declare -a ARGS=(
+    "--use-curriculum --use-boosting --use-neural"
+    "--use-boosting --use-neural"
+    "--use-curriculum --use-neural"
+    "--use-neural"
+)
+
+python run.py --n-exp 1 --n-jobs 1 ${ARGS[$SLURM_ARRAY_TASK_ID]}

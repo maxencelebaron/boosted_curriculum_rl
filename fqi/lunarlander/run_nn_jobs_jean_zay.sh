8189ba995389f0797cf78d733b876bebff56c09d
Pako Maxence TEKOU <tekoumaxencelebaron@gmail.com>
2026-08-20 16:34:15 +0200

    run neural version on jean_zay

---
+++ fqi/lunarlander/run_nn_jobs_jean_zay.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=neural_fqi_%a
+#SBATCH -C a100
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=20:00:00
+#SBATCH --output=slurm/logs/%x_%A_%a.out
+#SBATCH --error=slurm/logs/%x_%A_%a.err
+#SBATCH --array=0-3
+#SBATCH -A inl@a100
+
+module purge
+module load arch/a100
+
+export PYTHONPATH=$PYTHONPATH:$PWD/../..
+
+declare -a ARGS=(
+    "--use-curriculum --use-boosting --use-neural"
+    "--use-boosting --use-neural"
+    "--use-curriculum --use-neural"
+    "--use-neural --data-dir"
+)
+
+python run.py --n-exp 5 --monitor-loss ${ARGS[$SLURM_ARRAY_TASK_ID]}

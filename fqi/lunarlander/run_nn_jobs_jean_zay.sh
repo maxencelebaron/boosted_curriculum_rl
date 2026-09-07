d994abfae6a7b548b4b8e9ade8eace346821c613
Pako Maxence TEKOU <tekoumaxencelebaron@gmail.com>
2026-09-07 03:24:14 +0200

    optimize for jean-zay

--- fqi/lunarlander/run_nn_jobs_jean_zay.sh
+++ fqi/lunarlander/run_nn_jobs_jean_zay.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #SBATCH --job-name=dqn_lunarlander_%a
 #SBATCH -C a100
+#SBATCH --qos=qos_gpu_a100-dev
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=8
 #SBATCH --hint=nomultithread
-#SBATCH --time=8:00:00
+#SBATCH --time=01:55:00
 #SBATCH --output=slurm/logs/%x_%A_%a.out
 #SBATCH --error=slurm/logs/%x_%A_%a.err
-#SBATCH --array=0-17%5
+#SBATCH --array=0-17%10
 #SBATCH -A inl@a100
 
 module purge
